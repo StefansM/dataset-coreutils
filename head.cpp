f39f961754c2560dbf19aeb8935370fd77a55e8d
Stefans Mezulis <stefans.mezulis@gmail.com>
2022-03-12 14:14:23 +0000

    Add missing main files

---
+++ head.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <json/json.h>
+
+#include "query.h"
+#include "queryplan.h"
+#include "serde.h"
+#include "options.h"
+
+#define PROJECT_NAME "bash-functions"
+
+#include <boost/program_options.hpp>
+
+class HeadOptions : public Options {
+public:
+    HeadOptions() {
+        namespace po = boost::program_options;
+
+        description_.add_options()
+            ("lines,n", po::value(&lines_)->default_value(10), "Number of results to include.")
+        ;
+        add_positional_argument("lines", 0, 1);
+    }
+
+    virtual bool parse(int argc, char **argv) override {
+        bool parent_result = Options::parse(argc, argv);
+        if (!parent_result) {
+            return parent_result;
+        }
+
+        return true;
+    }
+
+    std::uint32_t get_lines() const { return lines_; }
+
+private:
+    std::uint32_t lines_;
+};
+
+
+int main(int argc, char **argv) {
+    HeadOptions options;
+    if (!options.parse(argc, argv)) {
+        return 1;
+    }
+
+    auto query_plan = load_query_plan(std::cin);
+    if (!query_plan) {
+        std::cerr << "Unable to parse query plan from standard input." << std::endl;
+        return 1;
+    }
+
+    query_plan->limit.emplace(options.get_lines());
+
+    dump_query_plan(*query_plan, std::cout);
+    return 0;
+}
---
+++ sort.cpp
@@ -0,0 +1,62 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include <json/json.h>
+
+#include "query.h"
+#include "queryplan.h"
+#include "serde.h"
+#include "options.h"
+
+#define PROJECT_NAME "bash-functions"
+
+#include <boost/program_options.hpp>
+
+class SortOptions : public Options {
+public:
+    SortOptions() {
+        namespace po = boost::program_options;
+
+        description_.add_options()
+            ("field,f", po::value(&fields_)->composing(), "Field on which to sort.")
+            ("reverse,r", po::bool_switch(&reversed_), "Descending sort.")
+        ;
+        add_positional_argument("field", 1, -1);
+    }
+
+    virtual bool parse(int argc, char **argv) override {
+        bool parent_result = Options::parse(argc, argv);
+        if (!parent_result) {
+            return parent_result;
+        }
+
+        return true;
+    }
+
+    std::vector<std::string> get_fields() const { return fields_; }
+    bool reversed() const { return reversed_; }
+
+private:
+    std::vector<std::string> fields_;
+    bool reversed_ = false;
+};
+
+
+int main(int argc, char **argv) {
+    SortOptions options;
+    if (!options.parse(argc, argv)) {
+        return 1;
+    }
+
+    auto query_plan = load_query_plan(std::cin);
+    if (!query_plan) {
+        std::cerr << "Unable to parse query plan from standard input." << std::endl;
+        return 1;
+    }
+
+    query_plan->order.emplace(options.get_fields(), options.reversed());
+
+    dump_query_plan(*query_plan, std::cout);
+    return 0;
+}
